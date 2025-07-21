acabf549c54c1818bf3cc23822958b7a0eafb076
Voydd9 <fabio.ferreira.2001@hotmail.com>
2025-07-21 17:27:54 +0100

    replaced Koyeb for Render as web service

--- src/wrapper/feature_flags/useFeatureFlags.tsx
+++ src/wrapper/feature_flags/useFeatureFlags.tsx
@@ -58,7 +58,7 @@ const useFeatureFlags = () => {
         ) {
           tmpHost = 'http://localhost:8000/';
         } else {
-          tmpHost = 'https://api-organized-poc.koyeb.app/';
+          tmpHost = 'https://organized-api-dev.onrender.com';
         }
       }
 
